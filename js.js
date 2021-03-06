c635b547bf69608de29b31db828504db9bce8997
Nico Apablaza <73095954+nicosuke@users.noreply.github.com>
2021-03-06 17:12:34 +1300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,25 @@
+/* PROM GET DIAMETRO
+DIVIDELO EN 2
+GUARDALO EN RAD
+PROM GET ALTURA
+GUARDALO EN ALT
+GET NUMERO DE CILINDROS
+
+RAD = RAD2;
+LET VOL = PI X RAD X ALT X CYL
+
+ESCRIBE VOL EN LA PANTALLA 
+*/
+function fn1() {
+ let bore = document.getElementById("bo").value;
+ let stroke = document.getElementById("st").value;
+ let cyl = document.getElementById("cy").value;
+let rad = (bore /2 );
+let vol = Math.PI * rad * rad * stroke * cyl / 1000;
+
+
+let sentence = `<h1>${vol}</h1>`
+document.querySelector('main').innerHTML = sentence;
+
+
+}
